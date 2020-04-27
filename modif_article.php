54802d2f3e6b84aeaa234f10e50d49caa4ae3dea
Luckian <luckian.olier@laposte.net>
2020-04-27 17:04:19 +0200

    suppr article

--- modif_article.php
+++ modif_article.php
@@ -18,24 +18,23 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
     $sql = 'SELECT * FROM `articles` WHERE `id` = :id;';
 
     $query = $db->prepare($sql);
-        //On injecte les valeurs dans la requete
-        $query->bindValue(':id', $id, PDO::PARAM_INT);
-        //On exécute la requete
-        $query->execute();
-        $article = $query->fetch(PDO::FETCH_ASSOC);
-        if(!$article){
-            header('Location: admin_articles.php');
-        }
-//L'article existe on va chercher les categories dans lesquels il a été cocher
- $sql = 'SELECT * FROM `articles_categories`WHERE `articles_id` = :id;';
- $query = $db->prepare($sql);
- //On injecte les valeurs dans la requete
- $query->bindValue(':id', $id, PDO::PARAM_INT);
- //On exécute la requete
- $query->execute();
- $categoriesArticle = $query->fetchAll(PDO::FETCH_ASSOC);
-
-}else{
+    //On injecte les valeurs dans la requete
+    $query->bindValue(':id', $id, PDO::PARAM_INT);
+    //On exécute la requete
+    $query->execute();
+    $article = $query->fetch(PDO::FETCH_ASSOC);
+    if (!$article) {
+        header('Location: admin_articles.php');
+    }
+    //L'article existe on va chercher les categories dans lesquels il a été cocher
+    $sql = 'SELECT * FROM `articles_categories`WHERE `articles_id` = :id;';
+    $query = $db->prepare($sql);
+    //On injecte les valeurs dans la requete
+    $query->bindValue(':id', $id, PDO::PARAM_INT);
+    //On exécute la requete
+    $query->execute();
+    $categoriesArticle = $query->fetchAll(PDO::FETCH_ASSOC);
+} else {
     header('Location: admin_articles.php');
 }
 //On vérifie que le formulaire a été envoyé
@@ -49,57 +48,76 @@ if (isset($_POST) && !empty($_POST)) {
         //On récupère le nom de l'image dans la base de données 
         $nom = $article['featured_image'];
 
-//On vérifie si on a une image
-    if(isset($_FILES['image']) && !empty($_FILES['image']) && $_FILES['image']
-    ['error'] != 4){
-        //On récupère les données
-        $image = $_FILES['image'];
+        //On vérifie si on a une image
+        if (isset($_FILES['image']) && !empty($_FILES['image']) && $_FILES['image']['error'] != 4) {
+            //On récupère les données
+            $image = $_FILES['image'];
 
-        //On vérifie que le ttransfert s'est mal passé 'error'=0
-        if($image['error'] != 0){
+            //On vérifie que le ttransfert s'est mal passé 'error'=0
+            if ($image['error'] != 0) {
 
-            echo 'Une erreur s\'est produite, devinez laquelle. 0x6845328486';
-            die;
+                echo 'Une erreur s\'est produite, devinez laquelle. 0x6845328486';
+                die;
+            }
+
+            //On limite les images aux png et jpg (jpeg aussi)
+            $types = ['image/png', 'image/jpeg'];
+
+            //On vérifie si le type du fichier est absent de la liste
+            if (!in_array($image['type'], $types)) {
+                echo "Le type de fichier doit être une image jpg ou png";
+                die;
+            }
+            //Le transfert s'est bien déroulé on déplace l'image temporaire après lui avoir généré un nouveau nom 
+            //Générer un nom pour le fichier -> nom + extension
+            $extension = pathinfo($image['name'], PATHINFO_EXTENSION);
+            // On génère un nom "aléatoire"
+            $nom = md5(uniqid()) . '.' . $extension;
+
+            //On génère le nom complet vers le dossier de destination
+            $nomComplet = __DIR__ . '/uploads/' . $nom;
+            //On déplace le fichier 
+            if (!move_uploaded_file($image['tmp_name'], $nomComplet)) {
+                echo "Le fichier n'a pas été copié! ";
+                die;
+            }
+
+            thumb(300, $nom);
+            thumb(150, $nom);
+            thumb(400, $nom);
+            resizeImage($nom, 75);
+            //Thumb = thumbnail (timbrre poste)
+            // Prendre reg.jpg
+            // Générer une miniature carré de 200*200
+            // Son nom devra être reg-200x200.jpg
+
+            //On gère la suppr des anciennes images
+            // On récup la première parti du nom de fichier de l'ancienne image
+           if ($article ['featured_image'] != null){
+
+           
+            $debutNom = pathinfo($article['featured_image'], PATHINFO_FILENAME);
+
+            //On récupère la liste des fichiers dnas uploads
+            $fichiers = scandir(__DIR__ . '/uploads/');
+
+            //Vu que c'est un tableau on boucle
+            foreach($fichiers as $fichier){
+                //Si le nom du fichier commence par $debutnom
+                if(strpos($fichier, $debutNom) === 0){
+                    //On supprime le fichier
+                    unlink(__DIR__ . '/uploads/' . $fichier);
+                }
+            }
+            }
         }
-
-        //On limite les images aux png et jpg (jpeg aussi)
-        $types =['image/png', 'image/jpeg'];
-
-       //On vérifie si le type du fichier est absent de la liste
-        if(!in_array($image['type'], $types)){
-            echo "Le type de fichier doit être une image jpg ou png";
-            die;
-        }
-        //Le transfert s'est bien déroulé on déplace l'image temporaire après lui avoir généré un nouveau nom 
-        //Générer un nom pour le fichier -> nom + extension
-        $extension = pathinfo($image['name'], PATHINFO_EXTENSION);
-        // On génère un nom "aléatoire"
-        $nom = md5(uniqid()) .'.' . $extension;
-        
-        //On génère le nom complet vers le dossier de destination
-        $nomComplet = __DIR__ . '/uploads/' . $nom;
-         //On déplace le fichier 
-         if(!move_uploaded_file($image['tmp_name'], $nomComplet)){
-            echo "Le fichier n'a pas été copié! ";
-            die;
-        }
-   
-thumb(300, $nom);
-thumb(150, $nom);
-thumb(400, $nom);
-resizeImage($nom, 75);
-        //Thumb = thumbnail (timbrre poste)
-// Prendre reg.jpg
-// Générer une miniature carré de 200*200
-// Son nom devra être reg-200x200.jpg
-}
 
 
 
 
 
         //On écrit la requête 
-        $sql = 'UPDATE `articles` SET `title` = :titre, `content` = :content, `featured_image` = :image WHERE `id` = :id,';
+        $sql = 'UPDATE `articles` SET `title` = :titre, `content` = :contenu, `featured_image` = :image WHERE `id` = :id;';
 
         //On prépare la requête
         $query = $db->prepare($sql);
@@ -114,34 +132,33 @@ resizeImage($nom, 75);
         //On exécute la requete
         $query->execute();
 
-    $sql = 'DELETE FROM `articles_categories` WHERE `articles_id` = :id';
-    $query = $db->prepare($sql);
-    //On injecte les valeurs dans la requete
-    $query->bindValue(':id', $id, PDO::PARAM_STR);
+        $sql = 'DELETE FROM `articles_categories` WHERE `articles_id` = :id';
+        $query = $db->prepare($sql);
+        //On injecte les valeurs dans la requete
+        $query->bindValue(':id', $id, PDO::PARAM_STR);
 
-    $query->execute();
+        $query->execute();
 
 
 
         //On récupère l'id de l'article nouvellement crée
-        $idArticle= $db->lastInsertId();
+        $idArticle = $db->lastInsertId();
 
         //On récupère dans post les cat cochées
         $categories = $_POST['categories'];
 
         //On ajoute les catégories
-        foreach($categories as $categorie){
+        foreach ($categories as $categorie) {
             //Ajouter les catégories
 
-    $sql = 'INSERT INTO `articles_categories`(`articles_id`, `categories_id`) VALUES (:idarticle, :idcategorie);';
-    $query = $db->prepare($sql);
-    $query->bindValue(':idarticle', $id, PDO::PARAM_INT);
-    $query->bindValue(':idcategorie', strip_tags($categorie), PDO::PARAM_INT);
-    $query->execute();
- 
+            $sql = 'INSERT INTO `articles_categories`(`articles_id`, `categories_id`) VALUES (:idarticle, :idcategorie);';
+            $query = $db->prepare($sql);
+            $query->bindValue(':idarticle', $id, PDO::PARAM_INT);
+            $query->bindValue(':idcategorie', strip_tags($categorie), PDO::PARAM_INT);
+            $query->execute();
         }
 
-        header('Location: index.php');
+        header('Location: admin_articles.php');
     } else {
         echo "Le fomulaire doit etre rempli complètement";
     }
@@ -174,27 +191,27 @@ resizeImage($nom, 75);
 
         <h2>Image</h2>
 
-    <div>
-        <label for="image">Fichier : </label>
-         <input type="file" name="image" id="image">       <!--//Un seul fichier / plusieur ajouter -> multiple <- apres id="ficher" -->
-    </div>
-    
-    
+        <div>
+            <label for="image">Fichier : </label>
+            <input type="file" name="image" id="image">
+            <!--//Un seul fichier / plusieur ajouter -> multiple <- apres id="ficher" -->
+        </div>
+
+
 
         <h2>Catégories</h2>
-        <?php foreach ($categories as $categorie):  
+        <?php foreach ($categories as $categorie) :
             $checked = '';
-            foreach ($categoriesArticle as $cat){
-                
-            if($cat['categories_id'] == $categorie['id']){   
-                $checked = 'checked';       
-            } 
-            //équivalent du if Condition ternaire -> $checked = ($cat['categories_id'] == $categorie['id']) ? 'checked' : '';
-        }
-            ?>
+            foreach ($categoriesArticle as $cat) {
+
+                if ($cat['categories_id'] == $categorie['id']) {
+                    $checked = 'checked';
+                }
+                //équivalent du if Condition ternaire -> $checked = ($cat['categories_id'] == $categorie['id']) ? 'checked' : '';
+            }
+        ?>
             <div>
-                <input type="checkbox" name="categories[]" id="cat_<?= $categorie['id'] ?>" 
-                value="<?= $categorie['id'] ?>" <?= $checked ?>>
+                <input type="checkbox" name="categories[]" id="cat_<?= $categorie['id'] ?>" value="<?= $categorie['id'] ?>" <?= $checked ?>>
                 <label for="categorie"> <?= $categorie['name'] ?> </label>
             </div>
 
--- suppr_article.php
+++ suppr_article.php
@@ -2,30 +2,63 @@
 //Ce fichier sert a suppr une catégorie
 
 if (isset($_GET['id']) && !empty($_GET['id'])) {
-    //On a un id et il n'est pas vide
-    $id = strip_tags($_GET['id']);
     //On va aller chercher la catégorie dans la base
+    $id = strip_tags($_GET['id']);
     //On se connecte
     require_once('inc/connect.php');
     //On écrit la requête sql
-    $sql = 'DELETE FROM `article` WHERE `id` = :id;';
+    $sql = 'SELECT * FROM `articles` WHERE `id` = :id;';
     //On prépare la requête
     $query = $db->prepare($sql);
     //On injecte les valeurs dans la requete
     $query->bindValue(':id', $id, PDO::PARAM_INT);
+    //On exécute la requete
+    $query->execute();
+    $article = $query->fetch(PDO::FETCH_ASSOC);
+    
+    
+    if(!$article){
+        header('Location: admin_articles.php');
+    }
+    if($article['featured_image'] != null){
 
+           
+        $debutNom = pathinfo($article['featured_image'], PATHINFO_FILENAME);
+
+        //On récupère la liste des fichiers dnas uploads
+        $fichiers = scandir(__DIR__ . '/uploads/');
+      
+        //Vu que c'est un tableau on boucle
+        foreach($fichiers as $fichier){
+            //Si le nom du fichier commence par $debutnom
+            if(strpos($fichier, $debutNom) === 0){
+                //On supprime le fichier
+                unlink(__DIR__ . '/uploads/' . $fichier);
+            }
+        }
+        }
+        $sql = 'DELETE FROM `articles_categories` WHERE `articles_id` = :id;'; 
+        $query = $db->prepare($sql);
+    //On injecte les valeurs dans la requete
+    $query->bindValue(':id', $id, PDO::PARAM_INT);
+    
     //On exécute la requete
     $query->execute();
 
-    //On déconnecte la base
+    //Supprimer l'article
+    $sql = 'DELETE FROM `articles` WHERE `id` = :id;'; 
+    $query = $db->prepare($sql);
+    //On injecte les valeurs dans la requete
+    $query->bindValue(':id', $id, PDO::PARAM_INT);
+    
+    //On exécute la requete
+    $query->execute();
     require_once('inc/close.php');
-    //On redirige vers une autre page (liste des catégories par exemple)
-    header('Location: admin_article.php');
 
 } else {
     //pas d'id 
     //oN REDIRIGE LE VISITEUR
     //On redirige vers une autre page (liste des catégories par exemple)
-    header('Location: admin_article.php');
+    header('Location: admin_articles.php');
 }
 ?>
